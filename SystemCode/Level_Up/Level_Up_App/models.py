548d84bbc970b846948261dfeadb9b60263cf9ff
Raymond <35454616+raymondng76@users.noreply.github.com>
2019-07-24 13:19:37 +0800

    Add default values for user details

--- SystemCode/Level_Up/Level_Up_App/models.py
+++ SystemCode/Level_Up/Level_Up_App/models.py
@@ -1,5 +1,30 @@
 from django.db import models
 
+EDU_CHOICES = [
+    ('SECONDARY', 'Secondary'),
+    ('DIPLOMA', 'Diploma'),
+    ('ASSODEG', 'Associate Degree'),
+    ('BACHDEG', 'Bachelor Degree'),
+    ('MASTDEG', 'Master Degree'),
+    ('DOCTDEG', 'Doctoral Degree'),
+]
+
+CAREER_POSITION_CHOICES = [
+    ('ASSTSOFTENG', 'Assistant Software Engineer'),
+    ('ASSOSOFTENG', 'Associate Software Engineer'),
+    ('SOFTENG', 'Software Engineer'),
+    ('SENISOFTENG', 'Senior Software Engineer'),
+    ('STAFSOFTENG', 'Staff Software Engineer'),
+    ('PRINSOFTENG', 'Principle Software Engineer'),
+    ('SOFTMAN', 'Software Manager'),
+    ('SENISOFTMAN', 'Senior Software Manager'),
+    ('SOFTDIR', 'Software Director'),
+    ('SENSOFTDIR', 'Senior Software Director'),
+    ('SOFTVP', 'Vice President, Software'),
+    ('SENSOFTVP', 'Senior Vice President, Software'),
+    ('CTO', 'Chief Technology Officer'),
+]
+
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=100)
@@ -9,3 +34,11 @@ class User(models.Model):
 
 class Questionaire(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    eduLevel = models.CharField(max_length=200, choices=EDU_CHOICES)
+    yearsExp = models.IntegerField(default=0)
+    currPosition = models.CharField(max_length=200, choices=CAREER_POSITION_CHOICES)
+    careerGoal = models.CharField(max_length=200, choices=CAREER_POSITION_CHOICES)
+
+    def __str__(self):
+        return """User: {}, Highest Edu Level: {}, Years of working exp: {},
+                Current position: {}, Have career goal: {}""".format(str(User.name), eduLevel, str(yearsExp), currPosition, careerGoal)
--- SystemCode/Level_Up/Level_Up_App/views.py
+++ SystemCode/Level_Up/Level_Up_App/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from Level_Up_App.forms import NewUserForm
-from Level_Up_App.models import User
+from Level_Up_App.forms import NewUserForm, QuestionaireForm
+from Level_Up_App.models import User, Questionaire
 # Create your views here.
 def index(request):
     form = NewUserForm()
@@ -15,5 +15,13 @@ def index(request):
     return render(request, 'Level_Up_App/index.html', form_dict)
 
 def userdetails(request):
-    user_dict = {'username': User.name}
-    return render(request, 'Level_Up_App/userdetails.html', context=user_dict)
+    form = QuestionaireForm()
+    # username = request.Get.get('userForm')
+    # TODO: How to get username from index.html?
+    form_dict = {'username': 'USER', 'questionaire': form}
+    if request.method == 'POST':
+        form = QuestionaireForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+    return render(request, 'Level_Up_App/userdetails.html', context=form_dict)
