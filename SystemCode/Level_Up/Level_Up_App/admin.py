17e7cb1a86802b2f9d34309dcc81ff29e4c449ae
Raymond <35454616+raymondng76@users.noreply.github.com>
2019-08-03 09:55:38 +0800

    Populate Career and EduLevel db

--- SystemCode/Level_Up/Level_Up_App/admin.py
+++ SystemCode/Level_Up/Level_Up_App/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from Level_Up_App.models import User, Questionaire, Skill, Course
+from Level_Up_App.models import *
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Questionaire)
 admin.site.register(Skill)
 admin.site.register(Course)
+admin.site.register(EducationLevel)
+admin.site.register(CareerPosition)
