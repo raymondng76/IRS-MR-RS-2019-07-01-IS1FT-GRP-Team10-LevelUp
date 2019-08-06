be2023af13b97c080f4ec2eaeaecae43f1dc749c
Raymond <35454616+raymondng76@users.noreply.github.com>
2019-08-06 09:57:03 +0800

    Revert to use function view instead of class base view

--- SystemCode/Level_Up/Level_Up_App/forms.py
+++ SystemCode/Level_Up/Level_Up_App/forms.py
@@ -6,6 +6,7 @@ class NewUserForm(forms.ModelForm):
         model = User
         fields = '__all__'
 
+
 class QuestionaireForm(forms.ModelForm):
     eduLevel = forms.ModelChoiceField(label='Highest education level', queryset=EducationLevel.objects.all())
     currPosition = forms.ModelChoiceField(label='Current working position', queryset=CareerPosition.objects.all())
